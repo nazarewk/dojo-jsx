ec8b58a29829a8604c40be995ca1b3a0266c374c
Krzysztof Nazarewski <nazarewk@gmail.com>
2016-02-14 11:45:01 +0100

    changed set jsx/load as main

--- demo/app/app.js
+++ demo/app/app.js
@@ -1,6 +1,6 @@
 require([
     'react/react-dom',
-    'jsx/load!app/Comment'
+    'jsx!app/Comment'
 ], function (ReactDOM, Comment) {
     var container = document.getElementById('app');
     return ReactDOM.render(Comment(), container);
