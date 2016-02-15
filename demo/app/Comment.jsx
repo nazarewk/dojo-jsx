0ba131e3a0995cbcc7542d27158fcb5978177cc3
Krzysztof Nazarewski <nazarewk@gmail.com>
2016-02-15 14:50:55 +0100

    demo fix

--- demo/app/Comment.jsx
+++ demo/app/Comment.jsx
@@ -1,13 +1,8 @@
 define([
-    'react/react'
-], function (React) {
-    return React.createClass({
-        render: function () {
-            return (
-                <div className="commentBox">
-                    Hello, world! I am a CommentBox.
-                </div>
-            );
-        }
-    });
+], function () {
+  return () => (
+    <div className="commentBox">
+      Hello, world! I am a CommentBox.
+    </div>
+  );
 });
